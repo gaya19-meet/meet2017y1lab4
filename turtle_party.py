3359d020ae6801c9e4e43a74f0da28f210f305f3
gaya19-meet <gaya19@meet.mit.edu>
2017-07-26 18:42:15 +0300

    Stopping here

---
+++ turtle_party.py
@@ -0,0 +1,15 @@
+strawberries = 50
+is_weekend = True
+
+if is_weekend: #this checks if it is a weekend
+    if strawberries > 39:
+        print('fun1')
+    else:
+        print ('not fun1')
+else:
+    if strawberries > 39 and strawberries <61:
+        print('fun2')
+    else:
+        print ('not fun2')
+    
+    
