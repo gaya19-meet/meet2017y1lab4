f61bb2007860364d0292266f6f8022a1043c5124
gaya19-meet <gaya19@meet.mit.edu>
2017-08-02 22:54:29 +0300

    Stopping here

---
+++ fruit_sorter.py
@@ -0,0 +1,11 @@
+new_fruit = input ("what fruit am i storing? ")
+print(new_fruit)
+if new_fruit == 'apples':
+    print('Bin 1')
+elif new_fruit == 'oranges':
+    print('Bin 2')
+elif new_fruit == 'Olives':
+    print ('Bin 3')
+else:
+    print ('Error! I do not recognize this fruit!')
+    
