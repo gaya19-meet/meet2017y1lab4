2d7911f7bd035cfb6ae2e2d9084d9f8e4bd0e49d
gaya19-meet <gaya19@meet.mit.edu>
2017-08-10 18:34:32 +0300

    Stopping here

---
+++ caught_speeding.py
@@ -0,0 +1,3 @@
+if speed < 31 #'no ticket'
+if speed > 30 and speed < 51 #'no ticket'
+if speed > 50 #'big ticket'
--- turtle_party.py
+++ turtle_party.py
@@ -1,15 +1,9 @@
 strawberries = 50
 is_weekend = True
 
-if is_weekend: #this checks if it is a weekend
-    if strawberries > 39:
-        print('fun1')
-    else:
-        print ('not fun1')
+if strawberries > 39 and strawberries < 61:
+    print ('Not Fun')
+
 else:
-    if strawberries > 39 and strawberries <61:
-        print('fun2')
-    else:
-        print ('not fun2')
-    
+    print('Fun')
     
